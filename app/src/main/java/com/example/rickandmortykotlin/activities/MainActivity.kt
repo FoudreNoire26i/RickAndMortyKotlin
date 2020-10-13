d967076bc9a19f6ec47dd5ebcfd6302967b27813
Benj <benjamin.vicat.12@gmail.com>
2020-10-13 14:51:25 +0200

    (intro frag implemented)

--- app/src/main/java/com/example/rickandmortykotlin/activities/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortykotlin/activities/MainActivity.kt
@@ -3,15 +3,35 @@ package com.example.rickandmortykotlin.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.FrameMetrics
 import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
 import com.example.rickandmortykotlin.R
+import com.example.rickandmortykotlin.fragments.CharacterDetailsFragment
+import com.example.rickandmortykotlin.fragments.OtherFragment
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, CharacterDetailsFragment.newInstance())
+            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out, android.R.anim.fade_in, android.R.anim.fade_out)
+            .commit()
+
+        findViewById<Button>(R.id.button_fragment).setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out, android.R.anim.fade_in, android.R.anim.fade_out)
+                .replace(R.id.fragment_container, OtherFragment.newInstance())
+                .commit()
+        }
     }
 
     fun openListActivity(v : View){
--- app/src/main/java/com/example/rickandmortykotlin/fragments/OtherFragment.kt
+++ app/src/main/java/com/example/rickandmortykotlin/fragments/OtherFragment.kt
@@ -14,21 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CharacterDetailsFragment.newInstance] factory method to
+ * Use the [OtherFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CharacterDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class OtherFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,7 +25,7 @@ class CharacterDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character_details, container, false)
+        return inflater.inflate(R.layout.fragment_other, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -51,9 +40,10 @@ class CharacterDetailsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CharacterDetailsFragment.
+         * @return A new instance of fragment OtherFragment.
          */
         // TODO: Rename and change types and number of parameters
-        fun newInstance() = CharacterDetailsFragment()
+        @JvmStatic
+        fun newInstance() = OtherFragment()
     }
 }
